2ef087697c7432eb8111489f5ffe160f8313be5d
Bruno Nardini <megatroom3@gmail.com>
2024-08-13 21:42:53 -0300

    fix: update legacy queries

--- legacy/sisautoescola/index.php
+++ legacy/sisautoescola/index.php
@@ -1,69 +1,73 @@
 <?php
-ob_start(); session_start(); ob_end_clean();
-include_once("configuracao.php");
+ob_start();
+session_start();
+ob_end_clean();
+include_once ("configuracao.php");
 
 $mysql = new modulos_global_mysql();
 
 if (!isset($_SESSION["AUTH"])) {
-    $_SESSION["AUTH"] = false;
+  $_SESSION["AUTH"] = false;
 }
 
 if (!($_SESSION["AUTH"] === true)) {
-    if ($mysql->dbConfigIsOk()) {
-        $_SESSION["AUTH"] = true;
-    } else {
-        $_SESSION["AUTH"] = false;
-        include_once 'setup.php';
-        exit;
-    }
+  if ($mysql->dbConfigIsOk()) {
+    $_SESSION["AUTH"] = true;
+  } else {
+    $_SESSION["AUTH"] = false;
+    include_once 'setup.php';
+    exit;
+  }
 }
 
 $sistema = null;
 $fieldList = $mysql->select('*', 'sistema');
 if (is_array($fieldList)) {
-    foreach ($fieldList as $fieldArray) {
-        $sistema[$fieldArray["campo"]] = $fieldArray["valor"];
-    }
+  foreach ($fieldList as $fieldArray) {
+    $sistema[$fieldArray["campo"]] = $fieldArray["valor"];
+  }
 }
 
-if (isset ($sistema["tema"]) and strlen($sistema["tema"]) > 0) {
-    $theme = $sistema["tema"];
+if (isset($sistema["tema"]) and strlen($sistema["tema"]) > 0) {
+  $theme = $sistema["tema"];
 } else {
-    $theme = 'cupertino';
+  $theme = 'cupertino';
 }
-if (isset ($sistema["janela"]) and strlen($sistema["janela"]) > 0) {
-    $window = $sistema["janela"];
+if (isset($sistema["janela"]) and strlen($sistema["janela"]) > 0) {
+  $window = $sistema["janela"];
 } else {
-    $window = 2;
+  $window = 2;
 }
 
 $login = "";
 $authenticated = false;
-if (isset ($_SESSION["LOGIN"]) and strlen($_SESSION["LOGIN"]) > 0) {
-    $login = $_SESSION["LOGIN"];
-    $authenticated = true;
+if (isset($_SESSION["LOGIN"]) and strlen($_SESSION["LOGIN"]) > 0) {
+  $login = $_SESSION["LOGIN"];
+  $authenticated = true;
 } else {
-    include_once('script.php');
-    //include_once('backup.php');
+  include_once ('script.php');
+  //include_once('backup.php');
 }
 
 $lstTelas = null;
-if (isset ($login) and $login != "") {
-    if (strtolower($login) == 'admin') {
-        $lstTelas = $mysql->select(
-                'a.id, b.descricao as modulo, a.descricao as tela, a.endereco, a.icone',
-                'telas a, modulos b',
-                'a.idmodulo = b.id',
-                null,
-                'b.ordem, a.ordem');
-    } else {
-        $lstTelas = $mysql->select(
-                'distinct id, modulo, tela, icone, endereco, ordemmodulos, ordemtelas',
-                'vacessos',
-                "idusuario = '".$_SESSION["IDUSUARIO"]."' or padrao = 1",
-                null,
-                'ordemmodulos, ordemtelas');
-    }
+if (isset($login) and $login != "") {
+  if (strtolower($login) == 'admin') {
+    $lstTelas = $mysql->select(
+      'a.id, b.descricao as modulo, a.descricao as tela, a.endereco, a.icone',
+      'telas a, modulos b',
+      'a.idmodulo = b.id',
+      null,
+      'b.ordem, a.ordem'
+    );
+  } else {
+    $lstTelas = $mysql->select(
+      'distinct id, modulo, tela, icone, endereco, ordemmodulos, ordemtelas',
+      'vacessos',
+      "idusuario = '" . $_SESSION["IDUSUARIO"] . "' or padrao = 1",
+      null,
+      'ordemmodulos, ordemtelas'
+    );
+  }
 }
 
 // if ($lstTelas == null) {
@@ -74,209 +78,223 @@ if (isset ($login) and $login != "") {
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+
 <head>
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" /> 
-<title>Sistema de Auto-Escola - 4 Rodas</title>
-<link type="text/css" rel="stylesheet" href="css/<?php echo $theme ?>/jquery-ui-1.7.2.custom.css" />
-<link type="text/css" rel="stylesheet" href="css/default.css" />
-<link type="text/css" rel="stylesheet" href="css/global.css" /> 
-<link type="text/css" rel="stylesheet" href="css/ui.jqgrid.css" media="screen" />
-<link type="text/css" rel="stylesheet" href="css/ui.multiselect.css" media="screen"  />
-<link type="text/css" rel="stylesheet" href="css/fullcalendar.css" />
-<style type="text/css">
-html, body {
-    width: 100%; height: 100%;
-    border: 0; padding: 0; margin: 0;
- }
-</style>
-<script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
-<script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
-<script type="text/javascript" src="js/ui/ui.core.js"></script>
-<script type="text/javascript" src="js/ui/ui.accordion.js"></script>
-<script type="text/javascript" src="js/ui/ui.tabs.js"></script>
-<script type="text/javascript" src="js/jquery.layout.js"></script>
-<script type="text/javascript" src="js/grid.locale-pt-br.js"></script>
-<script type="text/javascript" src="js/jquery.jqGrid.min.js"></script>
-<script type="text/javascript" src="js/jquery.tablednd.js"></script>
-<script type="text/javascript" src="js/jquery.contextmenu.js"></script>
-<script type="text/javascript" src="js/fullcalendar.js"></script>
-<script type="text/javascript" src="js/fullcalendar.min.js"></script>
-<script type="text/javascript" src="js/gcal.js"></script>
-<script type="text/javascript" src="js/jquery.meio.mask.js" charset="utf-8"></script>
-<script type="text/javascript" src="js/jquery-fonteffect-1.0.0.min.js"></script>
-<script type="text/javascript" src="modulos/global/global.php"></script>
-<script type="text/javascript" src="modulos/global/funcoes.js"></script>
-<script type="text/javascript">
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+  <title>Sistema de Auto-Escola - 4 Rodas</title>
+  <link type="text/css" rel="stylesheet" href="css/<?php echo $theme ?>/jquery-ui-1.7.2.custom.css" />
+  <link type="text/css" rel="stylesheet" href="css/default.css" />
+  <link type="text/css" rel="stylesheet" href="css/global.css" />
+  <link type="text/css" rel="stylesheet" href="css/ui.jqgrid.css" media="screen" />
+  <link type="text/css" rel="stylesheet" href="css/ui.multiselect.css" media="screen" />
+  <link type="text/css" rel="stylesheet" href="css/fullcalendar.css" />
+  <style type="text/css">
+    html,
+    body {
+      width: 100%;
+      height: 100%;
+      border: 0;
+      padding: 0;
+      margin: 0;
+    }
+  </style>
+  <script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
+  <script type="text/javascript" src="js/ui/ui.core.js"></script>
+  <script type="text/javascript" src="js/ui/ui.accordion.js"></script>
+  <script type="text/javascript" src="js/ui/ui.tabs.js"></script>
+  <script type="text/javascript" src="js/jquery.layout.js"></script>
+  <script type="text/javascript" src="js/grid.locale-pt-br.js"></script>
+  <script type="text/javascript" src="js/jquery.jqGrid.min.js"></script>
+  <script type="text/javascript" src="js/jquery.tablednd.js"></script>
+  <script type="text/javascript" src="js/jquery.contextmenu.js"></script>
+  <script type="text/javascript" src="js/fullcalendar.js"></script>
+  <script type="text/javascript" src="js/fullcalendar.min.js"></script>
+  <script type="text/javascript" src="js/gcal.js"></script>
+  <script type="text/javascript" src="js/jquery.meio.mask.js" charset="utf-8"></script>
+  <script type="text/javascript" src="js/jquery-fonteffect-1.0.0.min.js"></script>
+  <script type="text/javascript" src="modulos/global/global.php"></script>
+  <script type="text/javascript" src="modulos/global/funcoes.js"></script>
+  <script type="text/javascript">
 
-    $(function() {
-        $("#accordion").accordion({
-            icons: {
-                    header: "ui-icon-circle-arrow-e",
-                    headerSelected: "ui-icon-circle-arrow-s"
-            }
-        });
+    $(function () {
+      $("#accordion").accordion({
+        icons: {
+          header: "ui-icon-circle-arrow-e",
+          headerSelected: "ui-icon-circle-arrow-s"
+        }
+      });
 
-<?php
-    if ($window == 1) {
-?>
+      <?php
+      if ($window == 1) {
+        ?>
         $("#tabs").tabs({ cache: true });
-<?php
-    } else {
+        <?php
+      } else {
 
         if ($authenticated) {
-?>
-        $("#windowTitle").html('Bem Vindo');
-        $.post('modulos/index.php', {}, function(data) {
+          ?>
+          $("#windowTitle").html('Bem Vindo');
+          $.post('modulos/index.php', {}, function (data) {
             $("#windowContent").html(data);
-        });
-<?php
+          });
+          <?php
         } else {
-?>
-        $("#windowTitle").html('Login');
-        $.post('modulos/usuarios/login.php', {}, function(data) {
+          ?>
+          $("#windowTitle").html('Login');
+          $.post('modulos/usuarios/login.php', {}, function (data) {
             $("#windowContent").html(data);
-        });
-<?php
+          });
+          <?php
         }
-    }
-?>
+      }
+      ?>
 
     });
 
-    $(document).ready(function(){
+    $(document).ready(function () {
 
-        $("#linkLogo").click(function(event){
-            novaAbaMenuPrincipal("0", "modulos/index.php", "Bemvindo");
-            event.preventDefault();
-        });
+      $("#linkLogo").click(function (event) {
+        novaAbaMenuPrincipal("0", "modulos/index.php", "Bemvindo");
+        event.preventDefault();
+      });
 
-        setWidowType("<?php echo $window; ?>");
+      setWidowType("<?php echo $window; ?>");
 
-        <?php
+      <?php
 
-        if ($authenticated) {
-            $lastModulo = "";
-            foreach($lstTelas as $tela) {
-                if ($lastModulo != $tela["modulo"]) {
-                    echo '$("#menulink'.$tela["id"].'").click(function(event){ ';
-                    echo 'if ($("#menu'.$tela["id"].'").css("display") == "none") {';
-                        echo '$(".subMenuTopToolbar").hide(); ';
-                        echo '$("#menu'.$tela["id"].'").show(); ';
-                    echo '} else {';
-                        echo '$(".subMenuTopToolbar").hide(); ';
-                    echo '}';
-                    echo 'event.preventDefault(); });';
-                    $lastModulo = $tela["modulo"];
-                }
-                echo '$("#submenu'.$tela["id"].'").click(function(event){ ';
-                    echo 'novaAbaMenuPrincipal('.$tela["id"].', "'.$tela["endereco"].'", "'.$tela["tela"].'"); ';
-                    echo '$(".subMenuTopToolbar").hide(); ';
-                echo 'event.preventDefault(); });';
+      if ($authenticated) {
+        $lastModulo = "";
+        foreach ($lstTelas as $tela) {
+          if ($lastModulo != $tela["modulo"]) {
+            echo '$("#menulink' . $tela["id"] . '").click(function(event){ ';
+            echo 'if ($("#menu' . $tela["id"] . '").css("display") == "none") {';
+            echo '$(".subMenuTopToolbar").hide(); ';
+            echo '$("#menu' . $tela["id"] . '").show(); ';
+            echo '} else {';
+            echo '$(".subMenuTopToolbar").hide(); ';
+            echo '}';
+            echo 'event.preventDefault(); });';
+            $lastModulo = $tela["modulo"];
+          }
+          echo '$("#submenu' . $tela["id"] . '").click(function(event){ ';
+          echo 'novaAbaMenuPrincipal(' . $tela["id"] . ', "' . $tela["endereco"] . '", "' . $tela["tela"] . '"); ';
+          echo '$(".subMenuTopToolbar").hide(); ';
+          echo 'event.preventDefault(); });';
 
-                echo '$("#btnIconTopToolbar'.$tela["id"].'").click(function(event){ ';
-                    echo 'novaAbaMenuPrincipal('.$tela["id"].', "'.$tela["endereco"].'", "'.$tela["tela"].'"); ';
-                echo 'event.preventDefault(); });';
-            }
+          echo '$("#btnIconTopToolbar' . $tela["id"] . '").click(function(event){ ';
+          echo 'novaAbaMenuPrincipal(' . $tela["id"] . ', "' . $tela["endereco"] . '", "' . $tela["tela"] . '"); ';
+          echo 'event.preventDefault(); });';
         }
+      }
 
-        ?>
+      ?>
 
-    });    
+    });
 
-</script>
+  </script>
 
 </head>
+
 <body>
 
-<?php
-if ($authenticated) {
-?>
+  <?php
+  if ($authenticated) {
+    ?>
 
-<div id="indexTopo" style="position:absolute; left: 10px; top: 10px; right: 10px; width:auto; height: 90px; padding: 5px;" class="ui-widget-content" >
-    <a href="#" id="linkLogo">
+    <div id="indexTopo"
+      style="position:absolute; left: 10px; top: 10px; right: 10px; width:auto; height: 90px; padding: 5px;"
+      class="ui-widget-content">
+      <a href="#" id="linkLogo">
         <img src="images/logo.png" width="200px" height="70px" style="border: 0;" />
-    </a>
-    <div style="position:absolute; left: 220px; top: 10px; right: 10px; width:auto; font-size: 14px;">
+      </a>
+      <div style="position:absolute; left: 220px; top: 10px; right: 10px; width:auto; font-size: 14px;">
         <div class="topToolBar">
-            <?php
+          <?php
 
-            $lastModulo = "";
-            foreach($lstTelas as $tela) {
-                if ($lastModulo != $tela["modulo"]) {
-                    if ($lastModulo != "") {
-                        echo '</div></div>';
-                    }
-                    echo '<td><div class="menuTopToolbar">';
-                    echo '<a href="#" id="menulink'.$tela["id"].'">'.$tela["modulo"].'</a>';
-                    echo '<div id="menu'.$tela["id"].'" class="subMenuTopToolbar ui-widget-content ui-corner-all">';
-                    $lastModulo = $tela["modulo"];
-                }
-                echo '<div class="subMenuTopToolbarLink"><a href="#" id="submenu'.$tela["id"].'">'.$tela["tela"].'</a></div>';
+          $lastModulo = "";
+          foreach ($lstTelas as $tela) {
+            if ($lastModulo != $tela["modulo"]) {
+              if ($lastModulo != "") {
+                echo '</div></div>';
+              }
+              echo '<td><div class="menuTopToolbar">';
+              echo '<a href="#" id="menulink' . $tela["id"] . '">' . $tela["modulo"] . '</a>';
+              echo '<div id="menu' . $tela["id"] . '" class="subMenuTopToolbar ui-widget-content ui-corner-all">';
+              $lastModulo = $tela["modulo"];
             }
-            echo '</div></div>';
+            echo '<div class="subMenuTopToolbarLink"><a href="#" id="submenu' . $tela["id"] . '">' . $tela["tela"] . '</a></div>';
+          }
+          echo '</div></div>';
 
-            ?>
+          ?>
         </div>
         <div style="clear: both"></div>
         <div>
-            <?php
-            if (is_array($lstTelas)) {
-                foreach ($lstTelas as $tela) {
-                    if (isset ($tela["icone"]) and $tela["icone"] != '') {
-                        echo '<a class="btnIconTopToolbar" id="btnIconTopToolbar'.$tela["id"].'" href="#">';
-                        echo '<img src="icones/'.$tela["icone"].'" alt="'.$tela["tela"].'" title="'.$tela["tela"].'" width="50px" height="50px" style="border: 0;" />';
-                        echo '</a>';
-                    }
-                }
+          <?php
+          if (is_array($lstTelas)) {
+            foreach ($lstTelas as $tela) {
+              if (isset($tela["icone"]) and $tela["icone"] != '') {
+                echo '<a class="btnIconTopToolbar" id="btnIconTopToolbar' . $tela["id"] . '" href="#">';
+                echo '<img src="icones/' . $tela["icone"] . '" alt="' . $tela["tela"] . '" title="' . $tela["tela"] . '" width="50px" height="50px" style="border: 0;" />';
+                echo '</a>';
+              }
             }
-            ?>
+          }
+          ?>
         </div>
-    </div>
-    <div style="text-align: right; font-size: 10pt; margin: 5px;">
+      </div>
+      <div style="text-align: right; font-size: 10pt; margin: 5px;">
         <?php echo $_SESSION["USUARIO_NOME"]; ?>
+      </div>
     </div>
-</div>
 
-<?php
+    <?php
     if ($window == 1) {
-?>
-<div id="tabs" style="position:absolute; left: 238px; right: 20px; min-height: 507px; ">
-	<ul>
-		<li><a href="modulos/global/index.php">Página Inicial</a></li>
-	</ul>
-</div>
-<?php
+      ?>
+      <div id="tabs" style="position:absolute; left: 238px; right: 20px; min-height: 507px; ">
+        <ul>
+          <li><a href="modulos/global/index.php">Página Inicial</a></li>
+        </ul>
+      </div>
+      <?php
     } else {
-?>
-<div id="windowBox" style="position:absolute; left: 10px; right: 10px; top: 120px; min-height: 507px; width:auto;" class="ui-dialog ui-widget ui-widget-content ui-corner-all">
-   <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">
-      <span id="windowTitle" class="ui-dialog-title">Bem Vindo</span>
-   </div>
-   <div class="ui-dialog-content ui-widget-content" id="windowContent">
-      <p>Carregando...</p>
-      <h1>OLaaaaaaaaa</h1>
-   </div>
-</div>
-<?php
+      ?>
+      <div id="windowBox" style="position:absolute; left: 10px; right: 10px; top: 120px; min-height: 507px; width:auto;"
+        class="ui-dialog ui-widget ui-widget-content ui-corner-all">
+        <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">
+          <span id="windowTitle" class="ui-dialog-title">Bem Vindo</span>
+        </div>
+        <div class="ui-dialog-content ui-widget-content" id="windowContent">
+          <p>Carregando...</p>
+        </div>
+      </div>
+      <?php
     }
 
-} else { // if (isset ($login) and strlen($login) > 0) {
+  } else { // if (isset ($login) and strlen($login) > 0) {
 
-?>
-<table width="100%" border="0"><tr><td align="center" valign="middle">
-<div id="windowBox" style="width:350px;margin-top: 80px;" class="ui-dialog ui-widget ui-widget-content ui-corner-all">
-   <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">
-      <span id="windowTitle" class="ui-dialog-title">Bem Vindo</span>
-   </div>
-   <div class="ui-dialog-content ui-widget-content" id="windowContent">
-      <p>Carregando...</p>
-   </div>
-</div>
-</td></tr></table>
-<?php
+    ?>
+    <table width="100%" border="0">
+      <tr>
+        <td align="center" valign="middle">
+          <div id="windowBox" style="width:350px;margin-top: 80px;"
+            class="ui-dialog ui-widget ui-widget-content ui-corner-all">
+            <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">
+              <span id="windowTitle" class="ui-dialog-title">Bem Vindo</span>
+            </div>
+            <div class="ui-dialog-content ui-widget-content" id="windowContent">
+              <p>Carregando...</p>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </table>
+    <?php
 
-}
+  }
 
-?>
+  ?>
 </body>
+
 </html>
--- legacy/sisautoescola/modulos/ctrcaixa/index.xml.php
+++ legacy/sisautoescola/modulos/ctrcaixa/index.xml.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../../configuracao.php");
+include_once ("../../configuracao.php");
 
 $pDataI = $_POST["datai"];
 $pDataF = $_POST["dataf"];
@@ -14,121 +14,122 @@ $mysql = new modulos_global_mysql();
 
 $dataInicioCaixa = $mysql->getValue("valor", null, 'sistema', "campo = 'datainiciocaixa'");
 $dataInicioCaixaValid = new DateTime($dataInicioCaixa);
-if (!isset ($dataInicioCaixa) or $dataInicioCaixa == "" or !is_valid_date(db_to_date($dataInicioCaixa))) {
-    echo '<h2>Defina a data início do caixa na configurações do sistema.</h2>';
-    exit;
+if (!isset($dataInicioCaixa) or $dataInicioCaixa == "" or !is_valid_date(db_to_date($dataInicioCaixa))) {
+  echo '<h2>Defina a data início do caixa na configurações do sistema.</h2>';
+  exit;
 }
 if ($dataInicioCaixaValid > $pDataValidI) {
-    echo '<h2>A data inicial é inferior a data de configuração do caixa ('.  db_to_date($dataInicioCaixa) .').<br />'.
-        'Escolha uma data inicial igual ou superior.</h2>';
-    exit;
+  echo '<h2>A data inicial é inferior a data de configuração do caixa (' . db_to_date($dataInicioCaixa) . ').<br />' .
+    'Escolha uma data inicial igual ou superior.</h2>';
+  exit;
 }
 
 ?>
 <table id="users" class="ui-widget ui-widget-content" cellpadding="5">
-    <thead>
-        <tr class="ui-widget-header ">
-            <td colspan="9" align="center">Caixas</td>
-        </tr>
-        <tr class="ui-widget-header " align="center">
-            <td>Data</td>
-            <td>Nome</td>
-            <td>Login</td>            
-            <td>Valor Contas a Receber</td>
-            <td>Valor do Caixa</td>
-            <td>Diferença</td>
-            <td>Ajuste</td>
-            <td>Valor do Caixa Corrigido</td>
-            <?php
-            if ($acesso->getFuncionalidade(1)) {
-                echo "<td>&nbsp;</td>";
-            }
-            ?>
-        </tr>
-    </thead>
-    <tbody>
+  <thead>
+    <tr class="ui-widget-header ">
+      <td colspan="9" align="center">Caixas</td>
+    </tr>
+    <tr class="ui-widget-header " align="center">
+      <td>Data</td>
+      <td>Nome</td>
+      <td>Login</td>
+      <td>Valor Contas a Receber</td>
+      <td>Valor do Caixa</td>
+      <td>Diferença</td>
+      <td>Ajuste</td>
+      <td>Valor do Caixa Corrigido</td>
+      <?php
+      if ($acesso->getFuncionalidade(1)) {
+        echo "<td>&nbsp;</td>";
+      }
+      ?>
+    </tr>
+  </thead>
+  <tbody>
     <?php
     $cor = '';
     $datacount = $pDataValidI;
     while ($datacount <= $pDataValidF) {
 
-        $where = null;
-        $where[] = "(a.id in ".
-            "(select b.idusuario from usuariosgrupousuario b, acesso c ".
-            "where c.idgrupousuario = b.idgrupousuario and c.idtela = 21) or a.id in (select idusuario from contasareceber))";
-        if ($pUsuario > 0) {
-            $where[] = "a.idusuario = '".$pUsuario."'";
-        }
-        $where = join(" and ", $where);
+      $where = null;
+      $where[] = "(a.id in " .
+        "(select b.idusuario from usuariosgrupousuario b, acesso c " .
+        "where c.idgrupousuario = b.idgrupousuario and c.idtela = 21) or a.id in (select idusuario from contasareceber))";
+      if ($pUsuario > 0) {
+        $where[] = "a.idusuario = '" . $pUsuario . "'";
+      }
+      $where = join(" and ", $where);
 
-        $rows = $mysql->select(
-                "e.id as idcaixa, a.nome, a.login, coalesce(sum(d.valor), 0) valorconta, e.valor as valorcaixa, e.ajuste",
-                "vusuarios a ".
-                    "left join contasareceber d on d.idusuario = a.id and d.data = DATE('".$datacount->format('Y-m-d')."') ".
-                    "left join caixa e on d.data = e.data and a.id = e.idusuario",
-                $where,
-                "group by a.nome, a.login, e.valor, e.ajuste",
-                'nome');
-        //echo $mysql->getMsgErro() ."<br><br>";
+      $rows = $mysql->select(
+        "e.id as idcaixa, a.nome, a.login, coalesce(sum(d.valor), 0) valorconta, e.valor as valorcaixa, e.ajuste",
+        "vusuarios a " .
+        "left join contasareceber d on d.idusuario = a.id and d.data = DATE('" . $datacount->format('Y-m-d') . "') " .
+        "left join caixa e on d.data = e.data and a.id = e.idusuario",
+        $where,
+        "group by e.id, a.nome, a.login, e.valor, e.ajuste",
+        'nome'
+      );
+      //echo $mysql->getMsgErro() ."<br><br>";
 
-        if (is_array($rows)) {
-            foreach ($rows as $row) {
-                echo '<tr '.$cor.'>';
-                echo '<td align="center">'.$datacount->format('d/m/Y').'</td>';
-                echo '<td>'.$row["nome"].'</td>';
-                echo '<td>'.$row["login"].'</td>';
-                echo '<td align="right">'.db_to_float($row["valorconta"]).'</td>';
-                if (!isset ($row["valorcaixa"]) or $row["valorcaixa"] == "") {
-                    echo '<td align="right">Caixa Aberto</td>';
-                    echo '<td align="right">Caixa Aberto</td>';
-                    echo '<td align="right">Caixa Aberto</td>';
-                    echo '<td align="right">Caixa Aberto</td>';
-                    echo '<td>&nbsp;</td>';
-                } else {
-                    $diferenca = $row["valorcaixa"] - $row["valorconta"];
-                    if ($diferenca < 0) {
-                        $corDiferenca = 'style="color:red;"';
-                    } else if ($diferenca > 0) {
-                        $corDiferenca = 'style="color:blue;"';
-                    } else {
-                        $corDiferenca = '';
-                    }
-                    echo '<td align="right">'.db_to_float($row["valorcaixa"]).'</td>';
-                    echo '<td align="right" '.$corDiferenca.'>'.db_to_float($diferenca).'</td>';
-                    echo '<td align="right">'.db_to_float($row["ajuste"]).'</td>';
-                    echo '<td align="right">'.db_to_float($row["valorcaixa"]-$row["ajuste"]).'</td>';
-                    if ($acesso->getFuncionalidade(1)) {
-                        echo '<td>';
-                        echo '<a href="#" id="btnAjuste">Ajustar Caixa</a>';
-                        echo '<input type="hidden" value="'.$row["idcaixa"].'" ?>';
-                        echo '</td>';
-                    }
-                }                
-                echo '</tr>';                
+      if (is_array($rows)) {
+        foreach ($rows as $row) {
+          echo '<tr ' . $cor . '>';
+          echo '<td align="center">' . $datacount->format('d/m/Y') . '</td>';
+          echo '<td>' . $row["nome"] . '</td>';
+          echo '<td>' . $row["login"] . '</td>';
+          echo '<td align="right">' . db_to_float($row["valorconta"]) . '</td>';
+          if (!isset($row["valorcaixa"]) or $row["valorcaixa"] == "") {
+            echo '<td align="right">Caixa Aberto</td>';
+            echo '<td align="right">Caixa Aberto</td>';
+            echo '<td align="right">Caixa Aberto</td>';
+            echo '<td align="right">Caixa Aberto</td>';
+            echo '<td>&nbsp;</td>';
+          } else {
+            $diferenca = $row["valorcaixa"] - $row["valorconta"];
+            if ($diferenca < 0) {
+              $corDiferenca = 'style="color:red;"';
+            } else if ($diferenca > 0) {
+              $corDiferenca = 'style="color:blue;"';
+            } else {
+              $corDiferenca = '';
             }
+            echo '<td align="right">' . db_to_float($row["valorcaixa"]) . '</td>';
+            echo '<td align="right" ' . $corDiferenca . '>' . db_to_float($diferenca) . '</td>';
+            echo '<td align="right">' . db_to_float($row["ajuste"]) . '</td>';
+            echo '<td align="right">' . db_to_float($row["valorcaixa"] - $row["ajuste"]) . '</td>';
+            if ($acesso->getFuncionalidade(1)) {
+              echo '<td>';
+              echo '<a href="#" id="btnAjuste">Ajustar Caixa</a>';
+              echo '<input type="hidden" value="' . $row["idcaixa"] . '" ?>';
+              echo '</td>';
+            }
+          }
+          echo '</tr>';
         }
+      }
 
-        if ($cor == '') {
-            $cor = 'style="background:#D4D4D4;"';
-        } else {
-            $cor = '';
-        }
+      if ($cor == '') {
+        $cor = 'style="background:#D4D4D4;"';
+      } else {
+        $cor = '';
+      }
 
-        $datacount->add(new DateInterval('P1D'));
+      $datacount->add(new DateInterval('P1D'));
     }
     ?>
-    </tbody>
+  </tbody>
 </table>
 <script type="text/javascript">
-    $(document).ready(function(){
-        $("#btnAjuste").live('click', function(event) {
-            novaAbaMenuPrincipalComParametro(
-                'modulos/ctrcaixa/ajuste.php',
-                {
-                    id : $(this).next().val()
-                },
-                "Ajuste");
-            event.preventDefault();
-        });
+  $(document).ready(function () {
+    $("#btnAjuste").live('click', function (event) {
+      novaAbaMenuPrincipalComParametro(
+        'modulos/ctrcaixa/ajuste.php',
+        {
+          id: $(this).next().val()
+        },
+        "Ajuste");
+      event.preventDefault();
     });
+  });
 </script>
